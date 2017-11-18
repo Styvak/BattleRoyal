a2c536beef1f0265af038bd95852162257c6877c
Nicolas Gascon <nicolasgascon@nicolass-macbook-pro-4-1.home>
2017-11-18 18:06:00 +0100

    ?

---
+++ Assets/Scripts/Player/PlayerShoot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour {
+
+    private bool isAiming = false;
+
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Mouse1)) {
+            isAiming = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            isAiming = false;
+        }
+	}
+}
