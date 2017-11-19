ac84ba5cf8d78ea6bd45580ae7084d88ede641f2
Nicolas Gascon <nicolasgascon@nicolass-macbook-pro-4-1.home>
2017-11-19 19:27:04 +0100

    start reload

---
+++ Assets/Scripts/Network/NetworkPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkPlayer : NetworkBehaviour {
+
+    [SerializeField] private PlayerController playerController;
+    [SerializeField] private MoveController moveController;
+    [SerializeField] private TransformSync transformSync;
+    [SerializeField] private PlayerShoot playerShoot;
+    [SerializeField] private Destructable destructable;
+
+	void Start () {
+        if (isLocalPlayer)
+        {
+            playerController.enabled = true;
+            moveController.enabled = true;
+            transformSync.enabled = true;
+            playerShoot.enabled = true;
+            destructable.enabled = true;
+        }
+	}
+}
--- Assets/Scripts/Weapons/AssaultRifle.cs
+++ Assets/Scripts/Weapons/AssaultRifle.cs
@@ -6,7 +6,7 @@ public class AssaultRifle : Shooter {
 
     public override bool Fire()
     {
-        var b = base.Fire();
+        base.Fire();
 
         if (canFire)
         {
